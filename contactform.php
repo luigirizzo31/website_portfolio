b7ded6fca40e7a09d1bc016154a3aebb8c7c492d
luigirizzo31 <luroma1998@gmail.com>
2020-03-16 09:38:40 +0100

    Create contactform.php

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['usermessage'];
+
+    $email_from = 'luigirizzo.design@gmail.com';
+    $email_subject = "New Form Submission";
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "luigirizzo.design@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+?>
