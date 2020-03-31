b6786a3f9aef3f8e83fbddc2f1edcb4e2bd838fe
luigirizzo31 <luroma1998@gmail.com>
2020-04-01 00:05:58 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,19 +1,23 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['usermessage'];
 
-    $email_from = 'luigirizzo.design@gmail.com';
-    $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
-
-    $to = "luigirizzo.design@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: index.html");
+    if(isset($_POST['submit']))
+    {
+        require 'phpmailer/PHPMailerAutoload.php';
+        $mail = new PHPMailer;
+        //smtp settings
+        $mail->isSMTP(); // send as HTML
+        $mail->Host = "smtp.gmail.com"; // SMTP servers
+        $mail->SMTPAuth = true; // turn on SMTP authentication
+        $mail->Username = "luigirizzo.design@gmail.com"; // Your mail
+        $mail->Password = ''; // Your password mail
+        $mail->Port = 587; //specify SMTP Port
+        $mail->SMTPSecure = 'tls';                               
+        $mail->setFrom($_POST['email'],$_POST['name']);
+        $mail->addAddress('luigirizzo.design@gmail.com');
+        $mail->addReplyTo($_POST['email'],$_POST['name']);
+        $mail->isHTML(true);
+        $mail->Subject='Form Submission:' .$_POST['subject'];
+        $mail->Body='<h3>Name :'.$_POST['name'].'<br> Email: '.$_POST['email'].'<br>Message: '.$_POST['msg'].'</h3>';
+        
+    }
 ?>
